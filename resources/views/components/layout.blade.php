c9576791227174a9c5b8d05ed102f7302c0056b6
Serena Aoki <remiru103@gmail.com>
2026-02-19 20:36:45 +0900

    bootstrap途中(記事一覧)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,16 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js']) 
-    <link rel="stylesheet" href="{{ url('bootstrap.php') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     @include('layouts.navigation') 
-    <!-- ユーザー認証関連挙動テスト用 -->
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button type="submit">ログアウト</button>
-    </form>
+
     <div class="container">
         {{ $slot }}
     </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,18 +5,40 @@
         <h1>記事一覧</h1>
         <!-- trueなら投稿or削除時メッセージ表示 -->
         @if(session('message'))
-            {{ session('message')}}
+        {{ session('message')}}
         @endif
         <a href="{{ route('posts.create') }}">新しい投稿を作成</a>
-        <ul>
-            <!-- 投稿をループで表示 -->
-            @forelse ($posts as $post)
-            <li><a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a><p>{{ $post->created_at }} / {{ $post->user->name }}</p></li>
-            @empty <!-- 投稿がない場合 -->
-            <li>投稿された記事はありません</li>
-            @endforelse
-            <div class="pegination">
-                {{ $posts->links() }}
-            </div>
-        </ul>
+
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th class="w-50">タイトル</th>
+                    <th>投稿日</th>
+                    <th>投稿者</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <!-- 投稿をループで表示 -->
+                @forelse ($posts as $post)
+                    <tr>
+                        <td>
+                            <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+                        </td>
+                        <td>
+                            {{ $post->created_at }}
+                        </td>
+                        <td>
+                            <a href="{{ route('user.show', $post->user) }}">{{ $post->user->name }}</a>
+                        </td>
+                    </tr>    
+                @empty <!-- 投稿がない場合 -->
+                    <td>投稿された記事はありません</td>
+                @endforelse
+            </tbody>
+        </table>    
+
+        <div class="pagination">
+            {{ $posts->links() }}
+        </div>
 </x-layout>
